d878672b4645b48bd46518af14f79781a3f22dac
ldj526 <59276421+ldj526@users.noreply.github.com>
2022-12-05 00:09:28 +0900

    feat: Add save button function
    Save it to memoViewModel and return to MainActivity.

--- app/src/main/java/com/android/example/memoapp/memowrite/MemoWriteActivity.kt
+++ app/src/main/java/com/android/example/memoapp/memowrite/MemoWriteActivity.kt
@@ -1,12 +1,42 @@
 package com.android.example.memoapp.memowrite
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.android.example.memoapp.MainActivity
 import com.android.example.memoapp.R
+import com.android.example.memoapp.databinding.ActivityMemoWriteBinding
+import com.android.example.memoapp.model.Memo
+import com.android.example.memoapp.viewmodel.MemoViewModel
+import com.android.example.memoapp.viewmodel.MemoViewModelFactory
 
 class MemoWriteActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMemoWriteBinding
+    private lateinit var memoViewModel: MemoViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_memo_write)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_memo_write)
+        memoViewModel =
+            ViewModelProvider(this, MemoViewModelFactory(application))[MemoViewModel::class.java]
+
+        binding.btnSave.setOnClickListener {
+            val title = binding.etTitle.text.toString()
+            val content = binding.etContent.text.toString()
+
+            if (TextUtils.isEmpty(title) || TextUtils.isEmpty(content)) {
+                Toast.makeText(applicationContext, "제목과 내용을 입력해주세요.", Toast.LENGTH_LONG).show()
+            } else {
+                val memo = Memo(0, title, content)
+                memoViewModel.addMemo(memo)
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
