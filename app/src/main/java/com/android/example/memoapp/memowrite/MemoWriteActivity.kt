0174f2e698d1e077b6bf7d0905d58f006d147a44
ldj526 <59276421+ldj526@users.noreply.github.com>
2022-12-04 17:55:04 +0900

    feat: Set MemoWriteActivity layout

---
+++ app/src/main/java/com/android/example/memoapp/memowrite/MemoWriteActivity.kt
@@ -0,0 +1,12 @@
+package com.android.example.memoapp.memowrite
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.android.example.memoapp.R
+
+class MemoWriteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_memo_write)
+    }
+}
