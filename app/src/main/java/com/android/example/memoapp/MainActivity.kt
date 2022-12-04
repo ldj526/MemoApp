d9e1103d274342b1fbe7749425bf109fb38a91ab
ldj526 <59276421+ldj526@users.noreply.github.com>
2022-12-05 00:27:18 +0900

    feat: Connect the viewModel and add additional functionality

--- app/src/main/java/com/android/example/memoapp/MainActivity.kt
+++ app/src/main/java/com/android/example/memoapp/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.android.example.memoapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import com.android.example.memoapp.databinding.ActivityMainBinding
+import com.android.example.memoapp.memowrite.MemoWriteActivity
+import com.android.example.memoapp.viewmodel.MemoViewModel
+import com.android.example.memoapp.viewmodel.MemoViewModelFactory
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var memoViewModel: MemoViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        memoViewModel =
+            ViewModelProvider(this, MemoViewModelFactory(application))[MemoViewModel::class.java]
+
+        binding.btnFab.setOnClickListener {
+            val intent = Intent(this, MemoWriteActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
