1b655ccf5eab2d0a26ba8276bf3225813f13287f
ldj526 <59276421+ldj526@users.noreply.github.com>
2022-12-05 23:08:14 +0900

    feat: Retrieve memo's information

--- app/src/main/java/com/android/example/memoapp/UpdateActivity.kt
+++ app/src/main/java/com/android/example/memoapp/UpdateActivity.kt
@@ -1,11 +1,41 @@
 package com.android.example.memoapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.Window
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.android.example.memoapp.databinding.ActivityUpdateBinding
+import com.android.example.memoapp.model.Memo
+import com.android.example.memoapp.viewmodel.MemoViewModel
+import com.android.example.memoapp.viewmodel.MemoViewModelFactory
 
 class UpdateActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityUpdateBinding
+    private lateinit var memoViewModel: MemoViewModel
+    private lateinit var currentTitle: String
+    private lateinit var currentContent: String
+    private var currentId: Int? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_update)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_update)
+
+        memoViewModel =
+            ViewModelProvider(this, MemoViewModelFactory(application))[MemoViewModel::class.java]
+
+        if (intent.hasExtra("currentTitle") && intent.hasExtra("currentContent")) {
+            currentId = Integer.parseInt(intent.getStringExtra("currentId").toString())
+            currentTitle = intent.getStringExtra("currentTitle").toString()
+            currentContent = intent.getStringExtra("currentContent").toString()
+
+            binding.etTitle.setText(currentTitle)
+            binding.etContent.setText(currentContent)
+        } else {
+            Toast.makeText(this, "불러오기 실패", Toast.LENGTH_LONG).show()
+        }
     }
 }
