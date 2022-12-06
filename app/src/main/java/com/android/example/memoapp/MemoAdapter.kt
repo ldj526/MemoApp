f20f0eeafecd0fb73633c845c2aa0323d32cf898
ldj526 <59276421+ldj526@users.noreply.github.com>
2022-12-06 14:37:38 +0900

    refactor: Refactoring adapter

--- app/src/main/java/com/android/example/memoapp/MemoAdapter.kt
+++ app/src/main/java/com/android/example/memoapp/MemoAdapter.kt
@@ -10,8 +10,6 @@ import com.android.example.memoapp.model.Memo
 class MemoAdapter : RecyclerView.Adapter<MemoAdapter.ViewHolder>() {
     private var memoList = emptyList<Memo>()
 
-    class ViewHolder(val binding: ItemRecyclerviewBinding) : RecyclerView.ViewHolder(binding.root)
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding =
             ItemRecyclerviewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -19,22 +17,7 @@ class MemoAdapter : RecyclerView.Adapter<MemoAdapter.ViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val currentItem = memoList[position]
-        val currentId = currentItem.id.toString()
-        val currentTitle = currentItem.title
-        val currentContent = currentItem.content
-
-        holder.binding.tvId.text = currentId
-        holder.binding.tvContent.text = currentContent
-        holder.binding.tvTitle.text = currentTitle
-
-        holder.binding.itemLayout.setOnClickListener {
-            val intent = Intent(it.context, UpdateActivity::class.java)
-            intent.putExtra("currentId", currentId)
-            intent.putExtra("currentTitle", currentTitle)
-            intent.putExtra("currentContent", currentContent)
-            it.context.startActivity(intent)
-        }
+        holder.bind(memoList[position])
     }
 
     override fun getItemCount() = memoList.size
@@ -42,5 +25,26 @@ class MemoAdapter : RecyclerView.Adapter<MemoAdapter.ViewHolder>() {
     fun setData(memo: List<Memo>) {
         memoList = memo
         notifyDataSetChanged()
+    }
+
+    class ViewHolder(val binding: ItemRecyclerviewBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(currentItem: Memo) {
+            val currentId = currentItem.id.toString()
+            val currentTitle = currentItem.title
+            val currentContent = currentItem.content
+
+            with(binding) {
+                tvId.text = currentId
+                tvContent.text = currentContent
+                tvTitle.text = currentTitle
+                itemLayout.setOnClickListener {
+                    val intent = Intent(it.context, UpdateActivity::class.java)
+                    intent.putExtra("currentId", currentId)
+                    intent.putExtra("currentTitle", currentTitle)
+                    intent.putExtra("currentContent", currentContent)
+                    it.context.startActivity(intent)
+                }
+            }
+        }
     }
 }
